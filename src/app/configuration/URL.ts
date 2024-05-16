0a89dabbe3b48290fd5e3f03f520227f1d606cb2
czechp <webcoderc@gmail.com>
2024-05-16 21:20:56 +0200

    Set production mode

--- src/app/configuration/URL.ts
+++ src/app/configuration/URL.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080/api";
-// export const BACKEND_URL = "http://192.168.6.102:9010/api";
+// export const BACKEND_URL = "http://localhost:8080/api";
+export const BACKEND_URL = "http://192.168.6.102:9010/api";
