3d041dbad89bdd5ef19ce8362c56f991556e305d
czechp <webcoderc@gmail.com>
2024-11-18 17:14:19 +0100

    New DNS name for backend

--- src/app/configuration/URL.ts
+++ src/app/configuration/URL.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8080/api";
-export const BACKEND_URL = "http://192.168.6.102:9010/api";
+export const BACKEND_URL = "http://bsp-server-3.bispol.pl:9010/api";
